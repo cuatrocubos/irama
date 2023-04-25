0565ba7c9c431d01618f626fd6c0a4c1e9036895
Jorge Giron <jorgefgiron@Jorges-MacBook-Air.local>
2023-04-25 11:45:46 -0600

    removed sales consignment

--- irama/irama/page/sales_consignment/sales_consignment.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['sales-consignment'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Sales Consignment',
-		single_column: false
-	});
-}
